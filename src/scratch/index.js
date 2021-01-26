2d5cc71a8b1eed9184528ec38a7588e89b33d558
Andy Jakubowski <hello@andyjakubowski.com>
2021-01-26 14:52:05 +0100

    Do Things

--- src/scratch/index.js
+++ src/scratch/index.js
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
-const toggleMachine = createMachine({
-  id: 'toggle',
-  initial: 'off',
-  states: {
-    off: {
-      on: {
-        click: 'on',
+const toggleMachine = createMachine(
+  {
+    id: 'toggle',
+    initial: 'off',
+    states: {
+      off: {
+        entry: ['persistState'],
+        on: {
+          click: {
+            target: 'on',
+          },
+        },
       },
-    },
-    on: {
       on: {
-        click: 'off',
+        entry: ['persistState'],
+        on: {
+          click: {
+            target: 'off',
+          },
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      persistState: (context, event) => {
+        console.log('persistState');
+      },
+    },
+  }
+);
+
+const persistedState = JSON.parse(
+  localStorage.getItem('scratch') || toggleMachine.initialState
+);
 
 export const ScratchApp = () => {
-  const [state, send] = useMachine(toggleMachine, { devTools: true });
+  const [state, send] = useMachine(toggleMachine, {
+    devTools: true,
+    state: persistedState,
+    actions: {
+      persistState: (context, event) => {
+        console.log('YAS');
+      },
+    },
+  });
+
   return (
-    <div>
+    <div style={{ padding: '32px' }}>
       <p>
         Machine state: <span>{state.value}</span>
       </p>
