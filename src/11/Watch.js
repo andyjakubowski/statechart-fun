3101ebeb3b8f94316996aca44c4847df3b09e425
Andy Jakubowski <hello@andyjakubowski.com>
2021-02-07 12:11:40 +0100

    Add on-screen buttons

--- src/11/Watch.js
+++ src/11/Watch.js
@@ -37,10 +37,47 @@ const Watch = function Watch({ watchRef }) {
     <>
       <MetaInfo state={state} />
       <BeepLabel state={state} />
-      <Face state={state} />
+      <div className={cn('face-and-buttons')}>
+        <Face state={state} />
+        <WatchButton type="a" send={send}>
+          a
+        </WatchButton>
+        <WatchButton type="b" send={send}>
+          b
+        </WatchButton>
+        <WatchButton type="c" send={send}>
+          c
+        </WatchButton>
+        <WatchButton type="d" send={send}>
+          d
+        </WatchButton>
+      </div>
     </>
   );
 };
+
+const WatchButton = (function makeWatchButton() {
+  const types = ['a', 'b', 'c', 'd'];
+  const events = types.reduce((result, type) => {
+    result[type] = {
+      mouseDown: `${type.toUpperCase()}_PRESSED`,
+      mouseUp: `${type.toUpperCase()}_RELEASED`,
+    };
+    return result;
+  }, {});
+
+  return function WatchButton({ type, send, children: label }) {
+    return (
+      <button
+        onMouseDown={() => send(events[type].mouseDown)}
+        onMouseUp={() => send(events[type].mouseUp)}
+        className={cn(`button-${type}`)}
+      >
+        {label}
+      </button>
+    );
+  };
+})();
 
 const Face = function Face({ state }) {
   const isAlive = !!state && state.matches('alive');
